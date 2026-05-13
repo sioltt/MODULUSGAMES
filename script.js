4b88e4f01b056892c0e007ac44eaf40e93ad8e09
sioltt <gideonmartin84@gmail.com>
2026-05-12 19:30:25 -0700

    Add game loading functionality with URLs

---
+++ script.js
@@ -0,0 +1,10 @@
+const gameUrls = {
+    'bitlife': 'https://3kh0-assets.silvereen.net/3kh0-assets/bitlife/',
+    'retro-bowl': 'https://3kh0-assets.silvereen.net/3kh0-assets/retro-bowl/',
+    'slope': 'https://3kh0-assets.silvereen.net/3kh0-assets/slope/'
+};
+
+function loadGame(gameKey) {
+    const frame = document.getElementById('game-frame');
+    frame.src = gameUrls[gameKey];
+}
