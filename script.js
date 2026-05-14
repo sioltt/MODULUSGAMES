79fd9356ea4c821125d7e2fcf769213f0501ceb6
sioltt <gideonmartin84@gmail.com>
2026-05-13 17:20:10 -0700

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,14 @@ const gameUrls = {
 };
 
 function launchGame(gameKey) {
-    // Switch Screens
     document.getElementById('home-screen').classList.remove('active');
     document.getElementById('game-stage').classList.add('active');
-    
-    // Load Game
-    const frame = document.getElementById('game-frame');
-    frame.src = gameUrls[gameKey];
+    document.getElementById('game-frame').src = gameUrls[gameKey];
     document.getElementById('current-title').innerText = gameKey.toUpperCase();
 }
 
 function backToMenu() {
-    // Stop the game and switch back
-    const frame = document.getElementById('game-frame');
-    frame.src = ""; // Clears the game so it doesn't run in background
-    
+    document.getElementById('game-frame').src = "";
     document.getElementById('game-stage').classList.remove('active');
     document.getElementById('home-screen').classList.add('active');
 }
